f232617e16a84affe29b2f350f7382b6a35e956e
roprogramer91 <roprogramer91@gmail.com>
2025-10-27 08:06:31 -0300

    .

--- front/src/config.js
+++ front/src/config.js
@@ -1,10 +1,10 @@
 // config.js
 
 //export const API_BASE_URL = "http://localhost:3000"; // Para desarrollo BACK
-//export const AUTH_URL = 'http://localhost:5000'; // Para desarrollo AUTH
+export const AUTH_URL = 'http://localhost:5000'; // Para desarrollo AUTH
 
 
 export const API_BASE_URL = 'https://donaciones-back-dev-entorno.up.railway.app'; // Para entorno de desarrollo en Railway
-export const AUTH_URL = 'https://donaciones-auth-service-production.up.railway.app'; // Para entorno de desarrollo en Railway AUTH
+//export const AUTH_URL = 'https://donaciones-auth-service-production.up.railway.app'; // Para entorno de desarrollo en Railway AUTH
 
 
