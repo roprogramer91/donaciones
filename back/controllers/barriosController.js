32f0efd1b69d32fe8ee8f4d39f9e11fb73777b05
roprogramer91 <roprogramer91@gmail.com>
2025-10-31 02:56:12 -0300

    Se agrega ruta para get barrios by locaclidad , se agrega correspondiente controlador y modelo.

--- back/controllers/barriosController.js
+++ back/controllers/barriosController.js
@@ -33,7 +33,20 @@ async function getBarrioById(req, res) {
   }
 }
 
+async function getBarriosByLocalidad(req, res) {
+  const { id } = req.params;
+  try {
+    const barrios = await BarriosModel.getBarriosByLocalidadId(id);
+    res.json(barrios);
+  } catch (error) {
+    console.error('Error al obtener barrios por localidad:', error.message);
+    res.status(500).json({ error: 'Error al obtener barrio por localidad' });
+  }
+}
+
+
 module.exports = {
   getBarrios,
-  getBarrioById
+  getBarrioById,
+  getBarriosByLocalidad
 };
