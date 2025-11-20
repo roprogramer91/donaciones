898bbda657dc234c028b926f9b4f0f6ae743c97f
roprogramer91 <roprogramer91@gmail.com>
2025-11-20 20:47:41 -0300

    fix: usa utils/config en perfil de donante

--- front/src/donante/perfil-donante.js
+++ front/src/donante/perfil-donante.js
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from "../config.js";
+import { API_BASE_URL } from "../utils/config.js";
 import { appLogger } from "../utils/logger.js";
 
 const token = localStorage.getItem("token");
